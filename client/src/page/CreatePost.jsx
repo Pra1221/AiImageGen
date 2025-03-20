ddbab2799becbf9a62aae6dec0767a040d30b46a
Prachi <prachi639682@gmail.com>
2025-03-20 21:05:37 +0530

    new changes

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://aiimagegen-1.onrender.com', {
+        const response = await fetch('https://aiimagegen-1.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://aiimagegen-1.onrender.com', {
+        const response = await fetch('https://aiimagegen-1.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
